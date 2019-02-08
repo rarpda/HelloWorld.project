e3f87cda3416cd9c8fa75c684699acac172b5456
rarpda <ricardoarpa@hotmail.com>
2019-02-08 13:00:35 +0000

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void  main(String[] args){
+        HelloWorld.print();
+    }
+
+    public static String print(){
+        System.out.println("Hello World!");
+        return "Hello World!";
+    }
+
+
+}
---
+++ src/test/java/MyTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+import org.junit.Assert;
+public class MyTest {
+
+    @Test
+    public void name(){
+        Assert.assertEquals("Hello World", HelloWorld.print());
+    }
+
+
+}
